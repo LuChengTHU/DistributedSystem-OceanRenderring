e80c177be9fcc7ead0ecd55cebf6c5cbc80b844a
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2018-07-30 10:44:15 +0800

    Use maven

---
+++ render/src/main/java/renderer/tracer/RayTracerMapper.java
@@ -0,0 +1,31 @@
+package renderer.tracer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class RayTracerMapper
+    extends Mapper<Object, Text, Text, Text> {
+
+    RayTracer rayTracer = new RayTracer();
+
+//        rayTracer.setInput(new String("scene.txt"));
+//        rayTracer.setOutput(new String("pic.jpg"));
+
+
+}
